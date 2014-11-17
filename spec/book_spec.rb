c0e93aace9489c1734524b366239c6748eb7e1df
Vidya Raghavan <vidya@shippable.com>
2014-11-17 12:47:06 +0530

    Update book_spec.rb

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,18 +4,18 @@ require './book'
 describe Book, "#title" do
     it "returns the correct title" do
          book = Book.new
-        @book.title.should eql "title"
+        book.title.should eql "title"
     end
 end
 describe Book, "#author" do
    it "returns the correct author" do
     book = Book.new
-  @book.author.should eql "Author"
+  book.author.should eql "Author"
    end
 end
 describe Book, "#category" do
    it "returns the correct category" do
   book = Book.new
-  @book.category.should eql "category"
+  book.category.should eql "category"
    end
 end
