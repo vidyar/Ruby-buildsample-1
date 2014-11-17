7a49255633cf855fac8212fa5bd18c7f775d0832
Vidya Raghavan <vidya@shippable.com>
2014-11-17 12:05:54 +0530

    [skip ci]

--- bowling.rb
+++ bowling.rb
@@ -1,16 +1,8 @@
-class Bowling
-  describe "#title" do
-    it "returns the correct title" do
-        @<span class="skimlinks-unlinked">book.title.should</span> eql "Title"
-    end
-end
-describe "#author" do
-    it "returns the correct author" do
-        @book.author.should eql "Author"
-    end
-end
-describe "#category" do
-    it "returns the correct category" do
-        @book.category.should eql :category
-    end
+class Book
+    attr_accessor :title, :author, :category
+        def initialize title, author, category
+            @title = title
+            @author = author
+            @category = category
+        end
 end
