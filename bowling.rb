c6f7868b52ec3c8c21d11089ac2b9bf0d36eeecd
Vidya Raghavan <vidya@shippable.com>
2014-11-17 12:02:31 +0530

    [skip ci]

--- bowling.rb
+++ bowling.rb
@@ -1,8 +1,16 @@
 class Bowling
-  def hit(pins)
-  end
-
-  def score
-    0
-  end
+  describe "#title" do
+    it "returns the correct title" do
+        @<span class="skimlinks-unlinked">book.title.should</span> eql "Title"
+    end
+end
+describe "#author" do
+    it "returns the correct author" do
+        @book.author.should eql "Author"
+    end
+end
+describe "#category" do
+    it "returns the correct category" do
+        @book.category.should eql :category
+    end
 end
