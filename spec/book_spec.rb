74ab72f9af2261b733814d26e0f43310bd6f9039
vidyar <vidya@shippable.com>
2014-11-17 12:11:56 +0530

    updated book

---
+++ spec/book_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper.rb'
+require './book'
+
+describe "#title" do
+    it "returns the correct title" do
+        @<span class="skimlinks-unlinked">book.title.should</span> eql "Title"
+    end
+end
+describe "#author" do
+    it "returns the correct author" do
+        @book.author.should eql "Author"
+    end
+end
+describe "#category" do
+    it "returns the correct category" do
+        @book.category.should eql :category
+    end
+end
