1817bf889c08641c573bedc32ed9dedf1223f295
Vidya Raghavan <vidya@shippable.com>
2014-11-17 12:50:21 +0530

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,8 +1,20 @@
 class Book
-   attr_accessor :title, :author, :category
-        def initialize title, author, category
+   #attr_accessor :title, :author, :category
+        def initialize title
             @title = title1
+          #  @author = author1
+           # @category = category1
+        end
+end
+def initialize author
+           # @title = title1
             @author = author1
+            #@category = category1
+        end
+end
+def initialize category
+           # @title = title1
+           # @author = author1
             @category = category1
         end
 end
