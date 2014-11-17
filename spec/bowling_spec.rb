02b952e59efb279fcdf68137d3f0616304c63676
Vidya Raghavan <vidya@shippable.com>
2014-11-17 10:49:57 +0530

    Update bowling_spec.rb

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -5,6 +5,6 @@ describe Bowling, "#score" do
   it "returns 0 for all gutter game" do
     bowling = Bowling.new
     20.times { bowling.hit(0) }
-    bowling.score.should eq(0)
+    bowling.score.should eq(1)
   end
 end
